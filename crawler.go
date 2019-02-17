594a9445e8e599688409a2fbe1009ffe93e9dc76
Andrew Skurlatov <a.skurlatov@gmail.com>
2019-02-18 00:17:33 +0300

    Initial commit. Basic crawler

---
+++ crawler.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+	time2 "time"
+
+	"golang.org/x/net/html"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/sirupsen/logrus"
+)
+
+var target = "https://monzo.com/"
+var wg sync.WaitGroup
+var mu sync.RWMutex
+
+func main() {
+	entryUrl, err := url.Parse(target)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	siteMap := make(map[string]string)
+
+	siteMap[target] = ""
+
+	started := time2.Now()
+	wg.Add(1)
+	go CrawlUrl(*entryUrl, siteMap)
+
+	wg.Wait()
+
+	timeSpent := time2.Since(started)
+
+	logrus.Info(len(siteMap))
+	logrus.Info(timeSpent)
+
+}
+
+func CrawlUrl(url url.URL, siteMap map[string]string) error {
+	defer wg.Done()
+
+	resp, err := http.Get(url.String())
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	tokens := html.NewTokenizer(resp.Body)
+
+	for {
+		tokenType := tokens.Next()
+
+		switch tokenType {
+		case html.ErrorToken:
+			return nil
+		case html.StartTagToken, html.EndTagToken:
+			token := tokens.Token()
+			if "a" == token.Data {
+				for _, attr := range token.Attr {
+					if link := attr.Val; isLinkValid(link, target) && attr.Key == "href" {
+						childUrl, err := url.Parse(link)
+						if err != nil {
+							return err
+						}
+
+						mu.Lock()
+						_, ok := siteMap[childUrl.String()]
+						mu.Unlock()
+						if ok {
+							continue
+						}
+
+						mu.Lock()
+						siteMap[childUrl.String()] = ""
+						mu.Unlock()
+
+						spew.Dump(childUrl.String())
+						wg.Add(1)
+						//fmt.Println(siteMap)
+						go CrawlUrl(*childUrl, siteMap)
+					}
+				}
+			}
+		}
+	}
+}
+
+func isLinkValid(link, host string) bool {
+	if (strings.HasPrefix(link, "/") || strings.Contains(link, host)) && !strings.Contains(link, "email-protection") {
+		return true
+	}
+	return false
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/andskur/web-crawler
+
+require (
+	github.com/sirupsen/logrus v1.3.0 // indirect
+	github.com/vbauerster/mpb/v4 v4.4.0 // indirect
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
+)
