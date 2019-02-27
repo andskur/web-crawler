af689ebf1b0ff2e37e691f4b838ab19fa68e17a2
Andrew Skurlatov <a.skurlatov@gmail.com>
2019-02-28 02:13:00 +0300

    Command flags&args some refactoring

--- cmd/web-crawler.go
+++ cmd/web-crawler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -18,46 +19,59 @@ var (
 	errNoTarget = errors.New("no target url provided")
 )
 
-func main() {
-	target := getTarget()
+// TODO - better way move it to separate package or use Cobra-like external tools
 
+func main() {
+	// initialize target argument and flags
+	var target string
 	flagSet := flag.NewFlagSet("set", flag.ExitOnError)
 	fn := flagSet.String("fn", "", "-fn {filename} filename to write output")
 	mt := flagSet.String("mt", "hash", "-mt {hash || tree} sitemap type, hash map or page tree (default \"hash\")")
 	of := flagSet.String("of", "json", "-of {json || xml} output format, json or xml (default \"json\")")
 	v := flagSet.Bool("v", false, "-v verbose mode")
 
-	err := flagSet.Parse(os.Args[2:])
-	if err != nil {
-		logrus.Fatal(err)
+	// validate arguments
+	if len(os.Args) < 2 {
+		fmt.Println(usage)
+		flagSet.PrintDefaults()
+		os.Exit(1)
 	}
 
+	// get target from command-line argument
+	target = os.Args[1]
+	if target == "" {
+		fmt.Println(errNoTarget)
+		fmt.Println(usage)
+		flagSet.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// parse command-line flags
+	err := flagSet.Parse(os.Args[2:])
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(usage)
+		flagSet.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// create new Application Config from cli argument and params
 	cfg, err := config.NewConfig(target, *fn, *mt, *of, *v)
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
+	// create new Application with Config
 	app, err := application.NewApplication(cfg)
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
+	// start Crawling
 	app.StartCrawling()
 
+	// format Crawler output and write it to file
 	if err := app.WriteOutput(); err != nil {
 		logrus.Fatal(err)
 	}
-}
-
-// getTarget parse target URL from command lines argument
-func getTarget() (target string) {
-	if len(os.Args) < 2 {
-		logrus.Fatal(usage)
-		os.Exit(1)
-	}
-	target = os.Args[1]
-	if target == "" {
-		logrus.Fatal(errNoTarget)
-	}
-	return
 }
